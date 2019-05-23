6d62fcfb5af618990209440de761cee387aaf199
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-22 22:29:12 -0700

    we've done one answer from the first chapter

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def count_bits(x):
-    # TODO - you fill in here.
+    """ we can also create a cache """
     return 0
+
+def count_bits_one_at_a_time(x):
+    """ In order to count bits we can use the x & (x-1) trick"""
+    count = 0
+    while x:
+        x = x & (x-1)
+        count += 1
+
+    return count
 
 
 if __name__ == '__main__':
