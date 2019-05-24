4fa79909462d52c287bb03b5b940cd7bf1e0c819
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-23 17:54:37 -0700

    Did the parity problem

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,39 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    """ 
+    In order to solve this we will use the fact that the parity of
+    can be calculate by repeatedly cutting the word in half and XOR with
+    the two halfs.
+
+    An example of why is if we consider an 8 bit example 10101101
+
+    First you split it into 0101 1101 when XOR 
+        1010
+        1101
+        -----
+        0111
+
+    Then you split it into 10 01 when you XOR this you get 11
+        01
+        11
+        --
+        10
+    Finally you have 1 and 0 , you XOR this and get 0
+        0
+        1
+        -
+        1  
+    Thus the answer is a parity of 1.
+    """
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 0x01
+
 
 
 if __name__ == '__main__':
