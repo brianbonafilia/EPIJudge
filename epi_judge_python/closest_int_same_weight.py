3b0c9efb758fe0085eeb8c8b55ddcf06f311c75f
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-27 18:31:00 -0700

    closest_int_same_weight funding aquired

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,9 +1,32 @@
 from test_framework import generic_test
 
 
-def closest_int_same_bit_count(x):
+def closest_int_same_bit_count2(x):
     # TODO - you fill in here.
-    return 0
+    UNSIGNED_INT = 63
+    for i in range(0, UNSIGNED_INT):
+        if(x >> i) & 1 != (x >> i + 1) & 1:
+            x ^= (1 << i) | (1 << i + 1)
+            return x
+    return
+
+def closest_int_same_bit_count(x):
+    current_bit = 1
+    if x & 1:
+        current_bit <<= 1
+        while current_bit & x:
+            current_bit <<= 1
+        x ^= current_bit
+        current_bit >>= 1
+        x ^= current_bit 
+    else:
+        current_bit <= 1
+        while not current_bit & x:
+            current_bit <<= 1
+        x ^= current_bit
+        current_bit >>= 1
+        x ^= current_bit
+    return x
 
 
 if __name__ == '__main__':
--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,11 +1,6 @@
 from test_framework import generic_test
 
-
 def count_bits(x):
-    """ we can also create a cache """
-    return 0
-
-def count_bits_one_at_a_time(x):
     """ In order to count bits we can use the x & (x-1) trick"""
     count = 0
     while x:
