c9d36518655aa5b952326fcbc3513cc7101d546f
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-29 22:14:34 -0700

    primitive divide complete :)

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -1,12 +1,25 @@
 from test_framework import generic_test
 
+MAX_INT = 32
 
 def divide(x, y):
-    # TODO - you fill in here.
-    return 0
+    """  example x = 64, y = 3
+          x = 1000000 y = 11
+          when power goes from 32 -> 5
+          x >= y_power x = 64, y = 48
+    """      
+    result, power = 0, MAX_INT
+    while x >= y:
+        y_power = y << power 
+        if x >= y_power:
+            result += 1 << power
+            x -= y_power
+        power -= 1           
+    return result
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("primitive_divide.py",
-                                       "primitive_divide.tsv", divide))
+    divide(23, 5)
+    #exit(
+     #   generic_test.generic_test_main("primitive_divide.py",
+      #                                 "primitive_divide.tsv", divide))
