6f04502b294a5dea20b62d7cffbcaf586f839aa9
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-31 22:42:01 -0700

    add is_number_palindromic solution

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -2,11 +2,26 @@ from test_framework import generic_test
 
 
 def is_palindrome_number(x):
-    # TODO - you fill in here.
+    #
+    if x < 10:
+        return x >= 0
+    left_index = 10 ** (len(str(x))- 1)
+    right_index = 10
+    while left_index > 0:
+        left = x // left_index
+        if x // left_index != x % right_index:
+            return False
+        x -= left * left_index
+        x //= 10
+        left_index /= 100
     return True
+
+        
+    
 
 
 if __name__ == '__main__':
+    is_palindrome_number(74447)
     exit(
         generic_test.generic_test_main("is_number_palindromic.py",
                                        "is_number_palindromic.tsv",
