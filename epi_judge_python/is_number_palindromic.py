c46a6fce596f0f8dfb1f023500842ea143c369af
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-31 22:51:30 -0700

    use math class instead of len(str(x))

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,10 +1,11 @@
 from test_framework import generic_test
+import math
 
 
 def is_palindrome_number(x):
     if x < 10:
         return x >= 0
-    left_index = 10 ** (len(str(x))- 1)
+    left_index = 10 ** math.floor(math.log10(x))
     right_index = 10
     while left_index > 0:
         if x // left_index != x % right_index:
