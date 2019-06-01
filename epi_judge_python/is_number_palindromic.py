819ecc5a74e4aeb89479220e8fb14ba86cfd315d
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-31 22:47:49 -0700

    use another method of removing msd

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -2,16 +2,14 @@ from test_framework import generic_test
 
 
 def is_palindrome_number(x):
-    #
     if x < 10:
         return x >= 0
     left_index = 10 ** (len(str(x))- 1)
     right_index = 10
     while left_index > 0:
-        left = x // left_index
         if x // left_index != x % right_index:
             return False
-        x -= left * left_index
+        x %= left_index
         x //= 10
         left_index /= 100
     return True
