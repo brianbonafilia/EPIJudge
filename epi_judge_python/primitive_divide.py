1959fab70bbd13e1c65444e717e73d84c5749aa8
Brian_Bonafilia <bonafib@wwu.edu>
2019-05-31 20:24:23 -0700

    add solution to reverse_digits

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -4,9 +4,15 @@ MAX_INT = 32
 
 def divide(x, y):
     """  example x = 64, y = 3
-          x = 1000000 y = 11
-          when power goes from 32 -> 5
+          x = 0b1000000 y = 0b11
+          when power goes from 32 -> 16
           x >= y_power x = 64, y = 48
+          subtract 48 from 64 = 16 = x and result = 1 << 4 = 32
+          when power goes to 2,  y_power = 12 which is less than 16
+          result + (1 << 2) = 20
+          x = 4 and y = 3
+          result + 1 = 21   answer is 21.
+
     """      
     result, power = 0, MAX_INT
     while x >= y:
