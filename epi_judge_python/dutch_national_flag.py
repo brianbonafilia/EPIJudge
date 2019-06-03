a67dfdae8e3c37e26908f90be0087de4a96bd929
Brian_Bonafilia <bonafib@wwu.edu>
2019-06-03 14:22:19 -0700

    add solution to dutch national flag

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -8,8 +8,40 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index, A):
+    left, middle, right = 0, 0, len(A) - 1
+    pivot = A[pivot_index]
+    while middle <= right:
+        if A[middle] < pivot:
+            A[middle], A[left] = A[left], A[middle]
+            left += 1
+            middle += 1
+        elif A[middle] == pivot:
+            middle += 1
+        else:
+            A[middle], A[right] = A[right], A[middle]
+            right -= 1
+        
+
+def dutch_flag_partition_first_solution(pivot_index, A):
     # TODO - you fill in here.
-    return
+    i = 0
+    j = len(A) - 1
+    pivot = A[pivot_index]
+    while i <= j:
+        if A[i] < pivot:
+            i += 1
+        else:
+            A[i], A[j] = A[j], A[i]
+            j -= 1
+    j = len(A) - 1
+    while j >= i:
+        if A[j] > pivot:
+            j -= 1
+        else:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+
+
 
 
 @enable_executor_hook
@@ -39,6 +71,7 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
 
 
 if __name__ == '__main__':
+    dutch_flag_partition(5, [0, 2, 0, 0, 0, 1, 2, 0, 0, 0, 0])
     exit(
         generic_test.generic_test_main("dutch_national_flag.py",
                                        'dutch_national_flag.tsv',
