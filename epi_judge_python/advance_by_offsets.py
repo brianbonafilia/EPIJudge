9548504335897ab4644858910dc4cee63e242861
Brian_Bonafilia <bonafib@wwu.edu>
2019-06-03 21:35:39 -0700

    add solution for advance_by_offset

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -3,6 +3,13 @@ from test_framework import generic_test
 
 def can_reach_end(A):
     # TODO - you fill in here.
+    steps_can_take = 0
+    i = 0
+    while i < len(A) - 1:
+        steps_can_take = max(steps_can_take - 1, A[i])
+        if steps_can_take == 0:
+            return False
+        i += 1
     return True
 
 
