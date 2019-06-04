7c4eab575326cde9cb241ecb3e2c59631d7a8c0a
Brian_Bonafilia <bonafib@wwu.edu>
2019-06-03 20:41:36 -0700

    add optimal int as array multiplying solution

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -14,7 +14,8 @@ def multiply(num1, num2):
             result[i + j + 1] += num1[i] * num2[j]
             result[i + j] += result[i + j + 1] // 10
             result[i + j + 1] %= 10
-    # remove leading zeros
+    # remove leading zeros, if all or zeros and the result is
+    # None use [0] instead
     result = result[next((i for i, x in enumerate(result)
                           if x != 0), len(result)):] or [0]
     return [sign * result[0]] + result[1:]
