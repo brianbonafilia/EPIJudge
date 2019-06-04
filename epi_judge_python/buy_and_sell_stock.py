24a6d9d3ab4c60ecff2a835594755c4b5294d4a0
Brian_Bonafilia <bonafib@wwu.edu>
2019-06-03 22:50:03 -0700

    add solution to buy and sell stock once

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
 
-
 def buy_and_sell_stock_once(prices):
+    cur_min, largest_sell = prices[0], 0
+    for price in prices:
+        cur_min = min(cur_min, price)
+        largest_sell = max(largest_sell, price - cur_min)
+    return largest_sell
+
+
+def buy_and_sell_stock_once_original_solution(prices):
     # TODO - you fill in here.
-    return 0.0
+    cur_min = prices[0]
+    largest_sell = 0
+    for price in prices:
+        if price < cur_min:
+            cur_min = price
+        if price - cur_min > largest_sell:
+            largest_sell = price - cur_min
+    return largest_sell
 
 
 if __name__ == '__main__':
