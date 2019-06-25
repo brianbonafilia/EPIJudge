cbe2f0390efbe88b980c4ed77a4785a4e6950268
Brian_Bonafilia <bonafib@wwu.edu>
2019-06-24 18:50:44 -0700

    add solution for next permutation

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -3,7 +3,18 @@ from test_framework import generic_test
 
 def next_permutation(perm):
     # TODO - you fill in here.
-    return []
+    index = len(perm) - 2
+    while index >= 0:
+        if perm[index] < perm[index + 1]:
+            swap_index = len(perm) - 1
+            while perm[swap_index] <= perm[index]:
+                swap_index -= 1
+            perm[index], perm[swap_index] = perm[swap_index], perm[index]
+            return perm[:index+1] + list(reversed(perm[index+1:]))
+        index -= 1
+    return [] 
+            
+                
 
 
 if __name__ == '__main__':
